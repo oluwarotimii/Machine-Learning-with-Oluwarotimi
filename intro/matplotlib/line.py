47753e49da2afbbfdb9a2459abcd669beb99afd8
oluwarotimii <oluwarotimiadewumi@gmail.com>
2025-01-16 19:44:02 +0100

    plt: plotted the x against sinx on a line graph

--- intro/matplotlib/line.py
+++ intro/matplotlib/line.py
@@ -11,3 +11,10 @@ print("Y values are:\n", y)
 print("X values are:\n", x)
 
 # Create the plot
+plt.plot(x,y, label='sin(x)', color='blue', linestyle='-', linewidth=2)
+plt.title('Sine Wave')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+# plt.legend()
+plt.grid(True)
+plt.show()
