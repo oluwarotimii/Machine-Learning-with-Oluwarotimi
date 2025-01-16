8ba22c021943ae3dcfe245b74c669565dfeac7bc
oluwarotimii <oluwarotimiadewumi@gmail.com>
2025-01-16 19:39:11 +0100

    matplotlib: learning the use of matplotlib bibrary to  for data visualization

---
+++ intro/matplotlib/line.py
@@ -0,0 +1,13 @@
+# Making PLots  for data visualization
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# create data
+x =np.linspace(0,10,10)
+y = np.sin(x)
+
+print("Y values are:\n", y)
+print("X values are:\n", x)
+
+# Create the plot
